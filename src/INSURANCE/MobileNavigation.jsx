a0fdd22df488e4e896e74af0dbc4afb9c325d745
Afiniki-Mhya <johnafinikimhya12@gmail.com>
2022-08-23 12:52:07 +0100

    insurance dapp

---
+++ src/INSURANCE/MobileNavigation.jsx
@@ -0,0 +1,20 @@
+import NavLinks from "./NavLinks";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoCloseSharp } from "react-icons/io5";
+import { useState } from "react";
+const MobileNavigation = () => {
+const [open, setOpen] = useState(false);
+const menuIcon = <GiHamburgerMenu className="menu" onClick={() => setOpen(!open)} />
+const closeIcon = <IoCloseSharp  className="menu" onClick={() => setOpen(!open)} />
+
+const closeMoblieMenu = ( ) => setOpen(false);
+
+  return (
+    <nav className="MobileNavigation">
+     {open ? closeIcon : menuIcon}
+      {open && <NavLinks isMobile = {true} closeMoblieMenu = {closeMoblieMenu} />}
+    </nav>
+  );
+};
+
+export default MobileNavigation;
